d5bc171e6523e3dd9f74cc0d9cce52ff970fbea0
VladimirKoshovec <koshavladmir@gmail.com>
2024-06-25 23:16:32 +0300

    Update Navbar.tsx

--- src/app/componenets/Navbar.tsx
+++ src/app/componenets/Navbar.tsx
@@ -1,11 +1,11 @@
 export default function Navbar () {
-    return (<header>
-        <nav>
-            <ul className="m-1">
-                <li>Програмне забезпечення</li>
-                <li>Послуги супрводу</li>
-                <li>Послуги розробки</li>
-                <li>Послуги впровадження</li>
+    return (<header className="border-b-2 border-blue-500 h-14">
+        <nav className="w-1/2 items-center flex-wrap">
+            <ul className="flex items-center justify-between flex-wrap px-16">
+                <li>Програми</li>
+                <li>Супрвід</li>
+                <li>Розробки</li>
+                <li>Впровадження</li>
                 <li>Про нас</li>
             </ul>
         </nav>
