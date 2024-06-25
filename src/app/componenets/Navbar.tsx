0aa7badcaa9acc3e74385a66c6951eb02af2f40f
VladimirKoshovec <koshavladmir@gmail.com>
2024-06-25 15:39:00 +0300

    start

---
+++ src/app/componenets/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Navbar () {
+    return (<header>
+        <nav className="">
+            <ul>
+                <li>Програмне забезпечення</li>
+                <li>Послуги супрводу</li>
+                <li>Послуги розробки</li>
+                <li>Послуги впровадження</li>
+                <li>Про нас</li>
+            </ul>
+        </nav>
+    </header>);
+}
